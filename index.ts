e73615dcfba6f0abe9b60d3104b388876cda7c8a
mitsuumi <Susie.Wu@sunbirddcim.com>
2023-04-30 23:34:39 +0800

    Using RxJS: Video Release Notification

--- index.ts
+++ index.ts
@@ -1,62 +1,28 @@
 import { fromEvent, Subject } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
-const startButton = document.querySelector('#start');
-const countButton = document.querySelector('#count');
-const errorButton = document.querySelector('#error');
-const completeButton = document.querySelector('#complete');
+// 被觀察目標
+const youtuber$ = new Subject();
 
-const statusLabel = document.querySelector('#status');
-const currentCounterLabel = document.querySelector('#currentCounter');
-const evenCounterLabel = document.querySelector('#evenCounter');
+// 影片1上架，尚未有觀察者
+youtuber$.next(1);
 
-let counter = 0;
-// 自訂subject來通知計數器數值改變
-let counter$: Subject<number>;
+const observerA = {
+  next: (id) => {
+    console.log(`我是觀察者 A，我收到影片 ${id} 上架通知了`);
+  },
+  error: () => {},
+  complete: () => {},
+};
 
-fromEvent(startButton, 'click').subscribe(() => {
-  counter$ = new Subject();
-  counter = 0;
-  statusLabel.innerHTML = '目前狀態：開始計數';
+const observerASubscription = youtuber$.subscribe(observerA);
+youtuber$.next(2);
 
-  // counter$.subscribe((data) => {
-  //   currentCounterLabel.innerHTML = `目前計數: ${data}`;
-  //   if (data % 2 == 0) {
-  //     evenCounterLabel.innerHTML = `偶數計數: ${data}`;
-  //   }
-  // });
-
-  counter$.subscribe((data) => {
-    currentCounterLabel.innerHTML = `目前計數：${data}`;
-  });
-  const evenCounter$ = counter$.pipe(filter((data) => data % 2 === 0));
-  evenCounter$.subscribe((data) => {
-    evenCounterLabel.innerHTML = `偶數計數：${data}`;
-  });
-
-  counter$.subscribe({
-    next: () => {},
-    error: (message) => {
-      statusLabel.innerHTML = `目前狀態：錯誤 -> ${message}`;
-    },
-    complete: () => {
-      statusLabel.innerHTML = '目前狀態：完成';
-    },
-  });
-
-  // 一開始送出的預設值
-  counter$.next(counter);
+const observerBSubscription = youtuber$.subscribe((id) => {
+  console.log(`我是觀察者 B，我收到影片 ${id} 上架通知了`);
 });
 
-fromEvent(countButton, 'click').subscribe(() => {
-  counter$.next(++counter);
-});
+youtuber$.next(3);
 
-fromEvent(errorButton, 'click').subscribe(() => {
-  const reason = prompt('輸入錯誤訊息');
-  counter$.error(reason || 'error');
-});
-
-fromEvent(completeButton, 'click').subscribe(() => {
-  counter$.complete();
-});
+observerBSubscription.unsubscribe();
+youtuber$.next(4);
